b9d1813b4c835ebf93d2ce16b575d7dbc4c0e8ca
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2021-12-28 13:46:29 -0300

    two commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- reponsive meta-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Industrial || Responsive BootStrap HTML 5 Template</title>
+    <title>ecohouseautomacao</title>
     <!-- Bootstrap-->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- animate css-->
